83a86f0c40ea784377e00a1733508602d25a9d27
Sergio Alonso Garcia <sergio.garcia@qudrio.com>
2018-01-26 21:17:11 -0600

    API Integration

---
+++ src/org/sergiogarcia/CurrentTimeZone.java
@@ -0,0 +1,102 @@
+package org.sergiogarcia;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+
+/*
+
+        Demo Format:
+
+        {
+          "status":"OK",
+          "message":"",
+          "countryCode":"US",
+          "countryName":"United States",
+          "zoneName":"America\/Chicago",
+          "abbreviation":"CST",
+          "gmtOffset":-21600,
+          "dst":"0",
+          "dstStart":1446361200,
+          "dstEnd":1457856000,
+          "nextAbbreviation":"CDT",
+          "timestamp":1454446991,
+          "formatted":"2016-02-02 21:03:11"
+        }
+
+*/
+
+public class CurrentTimeZone implements Serializable {
+    private String status;
+    private String message;
+    public String countryCode;
+    public String countryName;
+    public String zoneName;
+    public String abbreviation;
+    private int gmtOffset;
+    private String dst;
+    private int dstStart;
+    private int dstEnd;
+    private String nextAbbreviation;
+    private int timestamp;
+    public String formatted;
+
+
+    public CurrentTimeZone(String status, String message, String countryCode,
+                           String countryName, String zoneName, String abbreviation,
+                           int gmtOffset, String dst, int dstStart, int dstEnd,
+                           String nextAbbreviation, int timestamp, String formatted) {
+        this.status = status;
+        this.message = message;
+        this.countryCode = countryCode;
+        this.countryName = countryName;
+        this.zoneName = zoneName;
+        this.abbreviation = abbreviation;
+        this.gmtOffset = gmtOffset;
+        this.dst = dst;
+        this.dstStart = dstStart;
+        this.dstEnd = dstEnd;
+        this.nextAbbreviation = nextAbbreviation;
+        this.timestamp = timestamp;
+        this.formatted = formatted;
+    }
+
+
+
+
+    public CurrentTimeZone(HashMap aMapRepresentation){
+        this.status = (String)aMapRepresentation.get("status");
+        this.message = (String)aMapRepresentation.get("message");
+        this.countryCode = (String)aMapRepresentation.get("countryCode");
+        this.countryName = (String)aMapRepresentation.get("countryName");
+        this.zoneName = (String)aMapRepresentation.get("zoneName");
+        this.abbreviation = (String)aMapRepresentation.get("abbreviation");
+        Long gmtOffset = (Long)aMapRepresentation.get("gmtOffset");
+        this.gmtOffset = gmtOffset.intValue();
+        this.dst = (String)aMapRepresentation.get("abbreviation");
+        Long dstStart = (Long)aMapRepresentation.get("dstStart");
+        this.dstStart = dstStart.intValue();
+        Long dstEnd = (Long)aMapRepresentation.get("dstEnd");
+        this.dstEnd = dstEnd.intValue();
+        this.nextAbbreviation = (String)aMapRepresentation.get("nextAbbreviation");
+        Long timestamp = (Long)aMapRepresentation.get("timestamp");
+        this.timestamp = timestamp.intValue();
+        this.formatted = (String)aMapRepresentation.get("formatted");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        CurrentTimeZone other = (CurrentTimeZone) obj;
+
+
+        // Test Each Field
+        return true;
+    }
+
+}
--- src/org/sergiogarcia/Main.java
+++ src/org/sergiogarcia/Main.java
@@ -1,49 +1,89 @@
 package org.sergiogarcia;
 
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Scanner;
 
+import org.omg.CORBA.Current;
 import org.sergiogarcia.quickconnectfamily.json.JSONUtilities;
 
 
 public class Main {
+    private static String json_endpoint = "http://api.timezonedb.com/v2/get-time-zone?key=27WXQFIWV7UQ&format=json&by=zone&zone=";
 
-    public static String clean_json(String json_input) {
-        return json_input.replace(System.getProperty("line.separator"), "");
+    private static String read_url_string(String url_input_string) throws IOException {
+        System.out.println("Reading Data From: " + url_input_string);
+        URL url = new URL(url_input_string);
+        Scanner scan = new Scanner(url.openStream());
+        String content = new String();
+        while (scan.hasNext()) content += scan.nextLine();
+        scan.close();
+
+        return content;
     }
 
-    public static void test_json_file(String filepath) {
-        System.out.println("Testing: " + filepath);
-
+    public static String parse_json(String jsonString) {
+        // Object Classes
+        HashMap parsedJSONMap = null;
+        CurrentTimeZone readObject = null;
         try {
-
-            // Object Classes
-            HashMap parsedJSONMap = null;
-            TestObject readObject = null;
-
-            // Read String from file ...
-            String jsonString = new String(Files.readAllBytes(Paths.get(filepath)));
-            // jsonString = clean_json(jsonString);
-            System.out.println("Contents: " + jsonString);
-
-            // Parse String
-            parsedJSONMap = (HashMap)JSONUtilities.parse(jsonString);
-            readObject = new TestObject(parsedJSONMap);
-            System.out.println("Parsed Object: " + parsedJSONMap.toString());
-
-            // Stringify ...
-            System.out.println("Stringified Object JSON: " + JSONUtilities.stringify(readObject));
-
+            parsedJSONMap = (HashMap) JSONUtilities.parse(jsonString);
+            readObject = new CurrentTimeZone(parsedJSONMap);
+            return parsedJSONMap.toString();
         } catch (Exception e) {
-            System.out.println("Exception when parsing json file:  " + e.toString());
+            System.out.println("Failed to Parse Json String: " + e.toString());
+            return null;
         }
     }
 
+    public static CurrentTimeZone parse_json_timezone(String jsonString) {
+        // Object Classes
+        HashMap parsedJSONMap = null;
+        CurrentTimeZone readObject = null;
+        try {
+            parsedJSONMap = (HashMap) JSONUtilities.parse(jsonString);
+            readObject = new CurrentTimeZone(parsedJSONMap);
+            return readObject;
+        } catch (Exception e) {
+            System.out.println("Failed to Parse Json String: " + e.toString());
+            return null;
+        }
+    }
+
+
+
+
     public static void main(String[] args){
-        for(int i = 0; i < args.length; i++) {
-            System.out.println("\n\nArgument " + i + ": " + args[i]);
-            test_json_file(args[i].toString());
+
+        String[] time_zones = {"America/Chicago", "America/New_York", "Asia/Dubai", "Asia/Tokyo"};
+
+
+        for(String tz:time_zones) {
+            System.out.println("------------------------------------------------------------------------------------\n\n");
+            // First We gather the Hour
+            System.out.println("First We gather the Hour for " + tz + "\n\n");
+            try {
+                // Download
+                String json_time = read_url_string(json_endpoint + tz);
+                System.out.println("Response JSON: " + json_time + "\n\n");
+
+                // Parse
+                CurrentTimeZone tx = parse_json_timezone(json_time);
+
+                // Stringify
+                System.out.println("Stringified Object JSON: " + JSONUtilities.stringify(tx) + "\n\n");
+
+                // Output
+                System.out.println("Current Date/Time in: " + tx.zoneName + " is " + tx.formatted + "\n\n");
+
+                // Sleep
+                Thread.sleep(2000); // This should be enough time for the request to finish
+            } catch (Exception e) {
+                System.out.println("Failed to gather JSON data: " + e.toString());
+            }
         }
     }
 
