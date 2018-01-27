d951e944d487b24ec8827382610c68d3d8379165
Sergio Alonso Garcia <sergio.garcia@qudrio.com>
2018-01-26 21:18:23 -0600

    Latest and Greatest

--- src/org/sergiogarcia/Main.java
+++ src/org/sergiogarcia/Main.java
@@ -2,12 +2,9 @@ package org.sergiogarcia;
 
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Scanner;
 
-import org.omg.CORBA.Current;
 import org.sergiogarcia.quickconnectfamily.json.JSONUtilities;
 
 
